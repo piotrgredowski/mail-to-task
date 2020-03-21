73ad09807ba8d9f0e8a415d7913c8b63747c5af0
Piotr Grędowski <piotrgredowski@gmail.com>
2020-03-21 12:34:08 +0100

    Many initial settings
    - requirements
    - first working app ready to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'mail_to_task:app'
---
+++ mail_to_task/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+import requests
+
+app = Flask('mail_to_task')
+
+@app.route('/', methods=['POST'])
+def main():
+    msg = request.json['plain'].strip()
+
+    target = request.args['target']
+    requests.post(target, msg.encode('utf-8'))
+    return 'ok'
+
+
+if __name__ == '__main__':
+    app.run()
